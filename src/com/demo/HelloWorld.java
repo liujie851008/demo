9dac8e31f754ca1cfd875d2d41604cfed03fb8bb
Guy <Guy@192.168.153.1>
2020-02-14 12:40:05 +0800

    我的第一个git

---
+++ src/com/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+/**
+ * @author Guy
+ * @date 2020-02-14
+ * @description 
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWord");
+	}
+}
+
+
